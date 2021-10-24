d849a4e7cbf0196c4962b50c03cccf683c66e2dc
gerry <hkggerry@gmail.com>
2021-10-23 18:14:01 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = "bob"
+var bestCustomer = "not bob"
+const leastFavoriteCustomer = "Assignment to constant variable."
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    var bestCustomer = "maybe bob"
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer;
+}
